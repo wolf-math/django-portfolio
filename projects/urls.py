09853f6aebf1174eaba7b24b0a707c99420cce81
aaronwolf <aaron.wolf@gmail.com>
2020-02-16 15:23:23 +0200

    worked routes, created individual project page

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,5 @@ from projects import views
 
 urlpatterns = [
   path('', views.all_projects),
-  # path('test', views.project_list)
+  path('<int:pk>', views.project_detail)
 ]
--- projects/views.py
+++ projects/views.py
@@ -11,3 +11,7 @@ def all_projects(request):
   # query db to return all project objects
   projects = Project.objects.all()
   return render(request, 'projects/all_projects.html', {'projects': projects})
+
+def project_detail(request, pk):
+  project = Project.objects.get(pk=pk)
+  return render(request, 'projects/detail.html', {'project': project})
